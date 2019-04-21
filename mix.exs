035bd713bdfad94470f30082f571a2bea51cadb3
Luis Lopez <luigibyte@gmail.com>
2019-04-21 11:18:12 -0300

    Add mqtt client library

--- mix.exs
+++ mix.exs
@@ -40,7 +40,8 @@ defmodule Dome.MixProject do
       {:postgrex, ">= 0.0.0"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:tortoise, "~> 0.9"}
     ]
   end
 
